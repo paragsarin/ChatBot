3bbb820a5caa6cf474f566411c7e3b179c778120
paragsarin <42388306+paragsarin@users.noreply.github.com>
2024-05-01 14:06:36 -0400

    Add files via upload

---
+++ Chat/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Json;
+
+using var client = new HttpClient();
+client.BaseAddress = new Uri("https://localhost:7166");
+while (true)
+{
+
+    Console.Write("Question: "); 
+    var question = Console.ReadLine();
+    await foreach (var item in client.GetFromJsonAsAsyncEnumerable<string>($"/ask?question={question}"))
+    { 
+        Console.Write(item);
+        await Task.Delay(250);
+    
+    }
+    Console.WriteLine();
+
+}
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
